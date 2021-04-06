23b0f02c8e00050492ae3ed67d9bfc553b0f0c1f
tsuchinaga <woocun@gmail.com>
2021-04-07 03:31:58 +0900

    clockのテストがciサーバのスペックによっては失敗するのに対応

--- infra/clock_test.go
+++ infra/clock_test.go
@@ -19,8 +19,8 @@ func Test_NewClock(t *testing.T) {
 func Test_clock_Now(t *testing.T) {
 	t.Parallel()
 	got := NewClock().Now()
-	want := time.Now()
-	if !reflect.DeepEqual(want, got) {
+	want := time.Now().Add(-time.Millisecond)
+	if !got.After(want) {
 		t.Errorf("%s error\nwant: %+v\ngot: %+v\n", t.Name(), want, got)
 	}
 }
